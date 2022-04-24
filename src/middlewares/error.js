523b6f210da916b1952fcd70e8b90f166e37e5dc
aptgetinfo <deepak.2002.02.16@gmail.com>
2022-04-24 13:11:24 +0530

    imporved error middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -4,18 +4,41 @@ const config = require('../config/config');
 const logger = require('../config/logger');
 const ApiError = require('../utils/ApiError');
 
-const errorConverter = (err, req, res, next) => {
-  let error = err;
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return message;
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  // eslint-disable-next-line security/detect-unsafe-regex
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `${value} already exists, Please use another value!`;
+  return message;
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return message;
+};
+
+exports.errorConverter = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+  if (error.name === 'CastError') error.message = handleCastErrorDB(error);
+  if (error.code === 11000) error.message = handleDuplicateFieldsDB(error);
+  if (error.name === 'ValidationError') error.message = handleValidationErrorDB(error);
   if (!(error instanceof ApiError)) {
     const statusCode =
       error.statusCode || error instanceof mongoose.Error ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
+    // eslint-disable-next-line security/detect-object-injection
     const message = error.message || httpStatus[statusCode];
     error = new ApiError(statusCode, message, false, err.stack);
   }
   next(error);
 };
 
-const errorHandler = (err, req, res, next) => {
+exports.errorHandler = (err, req, res, next) => {
   let { statusCode, message } = err;
   if (config.env === 'production' && !err.isOperational) {
     statusCode = httpStatus.INTERNAL_SERVER_ERROR;
@@ -27,6 +50,8 @@ const errorHandler = (err, req, res, next) => {
   const response = {
     code: statusCode,
     message,
+    status: err.status,
+    ...(config.env === 'development' && { error: err }),
     ...(config.env === 'development' && { stack: err.stack }),
   };
 
@@ -35,9 +60,4 @@ const errorHandler = (err, req, res, next) => {
   }
 
   res.status(statusCode).send(response);
-};
-
-module.exports = {
-  errorConverter,
-  errorHandler,
 };
